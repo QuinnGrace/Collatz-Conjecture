a940d976088224c2d457f64382c0b424d982a558
QuinnGrace <73368635+QuinnGrace@users.noreply.github.com>
2021-08-23 03:42:35 +0200

    init

--- package collatzmain;.java
+++ package collatzmain;.java
@@ -1,29 +1,39 @@
 package collatzmain;
 import java.util.*;
+import java.math.*;
 public class CollatzMain 
 {
     public static void main(String[] args) 
     {
         //input BigInteger
-        int testNumber;
-        int loopNumbers [] = {4, 2, 1};
+        //making a bigint array so comparisons can happen
+        int y = 3;
+        BigInteger loopNumbers [] = new BigInteger[y];
+        loopNumbers[0] = BigInteger.valueOf(4L);
+        loopNumbers[1] = BigInteger.valueOf(2L);
+        loopNumbers[2] = BigInteger.valueOf(1L);
+        //get the number to compare
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter the number to be tested -> ");
-        testNumber = input.nextInt();
-
+        BigInteger testNumber = input.nextBigInteger();
+       
+        //while the number being tested is not endlessly looping
         for(int x = 0; x < loopNumbers.length; ++x)
             while (testNumber != loopNumbers[x])
             {
-                if(testNumber % 2 == 0)
+                //if it is an even number, divide by 2
+                if(testNumber.mod(BigInteger.valueOf(2L))  == BigInteger.ZERO)
                 {
-                    testNumber = testNumber / 2;
+                    testNumber = testNumber.divide(BigInteger.valueOf(2L));
                     System.out.println(testNumber);
                 }
                 else
                 {
-                    testNumber = (3 * testNumber + 1);
+                    //if it is an odd number, multiply by 3 then add 1
+                    testNumber = testNumber.multiply(BigInteger.valueOf(3L));
+                    testNumber = testNumber.add(BigInteger.ONE);
                     System.out.println(testNumber);
                 }
-            }
+        
     }
 }
